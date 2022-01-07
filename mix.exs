4a3b96fe4ddaab9335daf4c3a32ba0f66cfae074
RoyalMist <thibault.fouache@gmail.com>
2022-01-07 22:52:42 +0100

    Config

--- mix.exs
+++ mix.exs
@@ -6,7 +6,10 @@ defmodule ExpoPush.MixProject do
       app: :expo_push,
       version: "0.0.1",
       elixir: "~> 1.13",
+      source_url: "https://github.com/RoyalMist/expo-push",
       start_permanent: Mix.env() == :prod,
+      description: description(),
+      package: package(),
       deps: deps()
     ]
   end
@@ -14,6 +17,18 @@ defmodule ExpoPush.MixProject do
   def application do
     [
       extra_applications: [:logger]
+    ]
+  end
+
+  defp description() do
+    "Lib to send Expo push notifications from an elixir application."
+  end
+
+  defp package() do
+    [
+      name: "expo_push",
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/RoyalMist/expo-push"}
     ]
   end
 
